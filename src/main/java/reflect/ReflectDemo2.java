2e8ccb051b1b9110466bacf4c8a2c306caa02e1c
Administrator <964188831@qq.com>
2021-03-04 14:51:13 +0800

    第三天

---
+++ src/main/java/reflect/ReflectDemo2.java
@@ -0,0 +1,26 @@
+package reflect;
+
+import java.util.Scanner;
+
+/**
+ * 使用反射机制实例化对象
+ */
+public class ReflectDemo2 {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Person p = new  Person();//硬编码形式实例化：new + 构造方法（）
+        System.out.println(p);
+
+        /*
+            java.util.Date 日期类
+         */
+        //1加载要实例化类的类对象
+//        Class cls = Class.forName("reflect.Person");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入类名：");
+        String className = scanner.nextLine();
+        Class cls = Class.forName(className);
+        //2通过类对象实例化
+        Object o = cls.newInstance();//表示的类必须有无参构造器
+        System.out.println(o);
+    }
+}
---
+++ src/main/java/reflect/Student.java
@@ -0,0 +1,11 @@
+package reflect;
+
+public class Student {
+    public void study(){
+        System.out.println("学生：好好学习，天天向上");
+    }
+
+    public void say(){
+        System.out.println("学生：啦啦啦啦啦~");
+    }
+}
